78b2ca39d5f42eafa4588ae823dd1c8c03f2e2e7
AllenChoiwonwoo <dkssudnjsdn@naver.com>
2024-03-01 17:50:01 +0900

    7장-연습 . 코드 정리

--- src/test/java/cahhp07/practice/ColumnParserTest.java
+++ src/test/java/cahhp07/practice/ColumnParserTest.java
@@ -10,14 +10,8 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ColumnParserTest {
-
-    @Test
-    void 컬럼파서_테스트(){
-
-    }
     private final String defaultColumns = "Dimension.DATE,Ad unit 1,Ad unit 2,Ad unit ID 1,Ad unit ID 2,Column.TOTAL_AD_REQUESTS,Column.TOTAL_LINE_ITEM_LEVEL_IMPRESSIONS,Column.TOTAL_LINE_ITEM_LEVEL_CLICKS,Column.TOTAL_LINE_ITEM_LEVEL_CPM_AND_CPC_REVENUE,Column.TOTAL_ACTIVE_VIEW_VIEWABLE_IMPRESSIONS_RATE";
     private final String exceptionColumnString = "Dimension.DATE,Ad unit 1,Ad unit 2,Ad unit 3,Ad unit ID 1,Ad unit ID 2,Ad unit ID 3,Column.TOTAL_AD_REQUESTS,Column.TOTAL_LINE_ITEM_LEVEL_IMPRESSIONS,Column.TOTAL_LINE_ITEM_LEVEL_CLICKS,Column.TOTAL_LINE_ITEM_LEVEL_CPM_AND_CPC_REVENUE,Column.TOTAL_ACTIVE_VIEW_VIEWABLE_IMPRESSIONS_RATE";
-
 
     private ColumnParser columnParser;
 
@@ -37,12 +31,7 @@ public class ColumnParserTest {
         assertEquals(7, columnsInfo.getClickIndex());
         assertEquals(8, columnsInfo.getRevenueIndex());
         assertEquals(9, columnsInfo.getViewablityIndex());
-    }
 
-    @Test
-    @DisplayName("기본순서와는 다른 순서를 가진 컬럼들에서 필요한 컬럼 파싱 테스트")
-    public void extractRequiredColumnFromExceptionColumns(){
-        // "Dimension.DATE,Ad unit 1,Ad unit 2,Ad unit 3,Ad unit ID 1,Ad unit ID 2,Ad unit ID 3,Column.TOTAL_AD_REQUESTS,Column.TOTAL_LINE_ITEM_LEVEL_IMPRESSIONS,Column.TOTAL_LINE_ITEM_LEVEL_CLICKS,Column.TOTAL_LINE_ITEM_LEVEL_CPM_AND_CPC_REVENUE,Column.TOTAL_ACTIVE_VIEW_VIEWABLE_IMPRESSIONS_RATE";
         ColumnsIndexInfo columnsIndexInfo = columnParser.getColumnIndex(exceptionColumnString);
         assertEquals(0, columnsIndexInfo.getDateIndex());
         assertEquals(3, columnsIndexInfo.getUnitNameIndex());
@@ -52,9 +41,5 @@ public class ColumnParserTest {
         assertEquals(9, columnsIndexInfo.getClickIndex());
         assertEquals(10, columnsIndexInfo.getRevenueIndex());
         assertEquals(11, columnsIndexInfo.getViewablityIndex());
-
-
     }
-
-
 }
--- src/test/java/cahhp07/practice/DefaultColumnParser.java
+++ src/test/java/cahhp07/practice/DefaultColumnParser.java
@@ -5,7 +5,6 @@ public class DefaultColumnParser implements ColumnParser {
     public ColumnsIndexInfo getColumnIndex(String columnString) {
         String[] columns = columnString.split(",");
 
-//     Dimension.DATE,Ad unit 1,Ad unit 2,Ad unit ID 1,Ad unit ID 2,Column.TOTAL_AD_REQUESTS,Column.TOTAL_LINE_ITEM_LEVEL_IMPRESSIONS,Column.TOTAL_LINE_ITEM_LEVEL_CLICKS,Column.TOTAL_LINE_ITEM_LEVEL_CPM_AND_CPC_REVENUE,Column.TOTAL_ACTIVE_VIEW_VIEWABLE_IMPRESSIONS_RATE";
         ColumnsIndexInfo result = new ColumnsIndexInfo();
         for (int i = 0; i < columns.length ; i++) {
             switch (columns[i]) {
@@ -23,13 +22,8 @@ public class DefaultColumnParser implements ColumnParser {
                     break;
             }
         }
-        if (result.getRequestIndex() == 5){
-            result.setUnitNameIndex(2);
-            result.setUnitIdIndex(4);
-        } else if (result.getRequestIndex() == 7) {
-            result.setUnitNameIndex(3);
-            result.setUnitIdIndex(6);
-        }
+        result.setUnitIdIndex((result.getRequestIndex()-1));
+        result.setUnitNameIndex(result.getUnitIdIndex()/2);
         return result;
     }
 }
