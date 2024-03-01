00503b9080cd149684a21ba013fa5d7815075fe2
AllenChoiwonwoo <dkssudnjsdn@naver.com>
2024-03-01 20:53:17 +0900

    7장-연습 . 상수명 변경

--- src/test/java/cahhp07/practice/ColumnParserTest.java
+++ src/test/java/cahhp07/practice/ColumnParserTest.java
@@ -1,17 +1,13 @@
 package cahhp07.practice;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.Arrays;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ColumnParserTest {
-    private final String defaultColumns = "Dimension.DATE,Ad unit 1,Ad unit 2,Ad unit ID 1,Ad unit ID 2,Column.TOTAL_AD_REQUESTS,Column.TOTAL_LINE_ITEM_LEVEL_IMPRESSIONS,Column.TOTAL_LINE_ITEM_LEVEL_CLICKS,Column.TOTAL_LINE_ITEM_LEVEL_CPM_AND_CPC_REVENUE,Column.TOTAL_ACTIVE_VIEW_VIEWABLE_IMPRESSIONS_RATE";
-    private final String exceptionColumnString = "Dimension.DATE,Ad unit 1,Ad unit 2,Ad unit 3,Ad unit ID 1,Ad unit ID 2,Ad unit ID 3,Column.TOTAL_AD_REQUESTS,Column.TOTAL_LINE_ITEM_LEVEL_IMPRESSIONS,Column.TOTAL_LINE_ITEM_LEVEL_CLICKS,Column.TOTAL_LINE_ITEM_LEVEL_CPM_AND_CPC_REVENUE,Column.TOTAL_ACTIVE_VIEW_VIEWABLE_IMPRESSIONS_RATE";
+    private final String DEFAULT_COLUMN_STRING = "Dimension.DATE,Ad unit 1,Ad unit 2,Ad unit ID 1,Ad unit ID 2,Column.TOTAL_AD_REQUESTS,Column.TOTAL_LINE_ITEM_LEVEL_IMPRESSIONS,Column.TOTAL_LINE_ITEM_LEVEL_CLICKS,Column.TOTAL_LINE_ITEM_LEVEL_CPM_AND_CPC_REVENUE,Column.TOTAL_ACTIVE_VIEW_VIEWABLE_IMPRESSIONS_RATE";
+    private final String EXCEPTION_COLUMN_STRING = "Dimension.DATE,Ad unit 1,Ad unit 2,Ad unit 3,Ad unit ID 1,Ad unit ID 2,Ad unit ID 3,Column.TOTAL_AD_REQUESTS,Column.TOTAL_LINE_ITEM_LEVEL_IMPRESSIONS,Column.TOTAL_LINE_ITEM_LEVEL_CLICKS,Column.TOTAL_LINE_ITEM_LEVEL_CPM_AND_CPC_REVENUE,Column.TOTAL_ACTIVE_VIEW_VIEWABLE_IMPRESSIONS_RATE";
 
     private ColumnParser columnParser;
 
@@ -23,7 +19,7 @@ public class ColumnParserTest {
     @Test
     public void extractRequiredColumn(){
         try {
-        ColumnsIndexInfo columnsInfo = columnParser.getColumnIndex(defaultColumns);
+        ColumnsIndexInfo columnsInfo = columnParser.getColumnIndex(DEFAULT_COLUMN_STRING);
             assertEquals(0, columnsInfo.getDateIndex());
             assertEquals(2, columnsInfo.getUnitNameIndex());
             assertEquals(4, columnsInfo.getUnitIdIndex());
@@ -33,7 +29,7 @@ public class ColumnParserTest {
             assertEquals(8, columnsInfo.getRevenueIndex());
             assertEquals(9, columnsInfo.getViewablityIndex());
 
-            ColumnsIndexInfo columnsIndexInfo = columnParser.getColumnIndex(exceptionColumnString);
+            ColumnsIndexInfo columnsIndexInfo = columnParser.getColumnIndex(EXCEPTION_COLUMN_STRING);
             assertEquals(0, columnsIndexInfo.getDateIndex());
             assertEquals(3, columnsIndexInfo.getUnitNameIndex());
             assertEquals(6, columnsIndexInfo.getUnitIdIndex());
@@ -55,10 +51,10 @@ public class ColumnParserTest {
 
     @Test
     public void allColumnIndexChecked() {
-        ColumnsIndexInfo columnsIndexInfo1 = columnParser.getColumnIndex(defaultColumns);
+        ColumnsIndexInfo columnsIndexInfo1 = columnParser.getColumnIndex(DEFAULT_COLUMN_STRING);
         assertEquals(true, columnsIndexInfo1.isAllColumnChecked());
 
-        ColumnsIndexInfo columnsIndexInfo2 = columnParser.getColumnIndex(exceptionColumnString);
+        ColumnsIndexInfo columnsIndexInfo2 = columnParser.getColumnIndex(EXCEPTION_COLUMN_STRING);
         assertEquals(true, columnsIndexInfo2.isAllColumnChecked());
 
         String valueForError = "Dimension,Ad unit 1,Ad unit 2,Ad unit 3,Ad unit ID 1,Ad unit ID 2,Ad unit ID 3,Column.TOTAL_AD_REQUESTS,Column.TOTAL_LINE_ITEM_LEVEL_IMPRESSIONS,Column.TOTAL_LINE_ITEM_LEVEL_CLICKS,Column.TOTAL_LINE_ITEM_LEVEL_CPM_AND_CPC_REVENUE,Column.TOTAL_ACTIVE_VIEW_VIEWABLE_IMPRESSIONS_RATE";
